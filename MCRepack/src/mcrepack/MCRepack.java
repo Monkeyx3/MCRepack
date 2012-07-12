0567dde6ff2e7e0e22d347b9d6995417884f0364
Monkey <monkey@mcwow.de>
2012-07-12 17:32:45 +0200

    init window

--- MCRepack/src/mcrepack/MCRepack.java
+++ MCRepack/src/mcrepack/MCRepack.java
@@ -1,9 +1,27 @@
 package mcrepack;
 
-public class MCRepack 
+import javax.swing.*;
+
+public class MCRepack extends JFrame
 {
+    public MCRepack()
+    {
+        initWindow();
+        setVisible(true);
+    }
+    
+    private void initWindow()
+    {
+        setSize(600,400);
+        setLocation(100,100);
+        setLayout(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("MCRepack v0.0.1");
+    }
+    
     public static void main(String[] args) 
     {
-        
+        new MCRepack();
     }
 }
